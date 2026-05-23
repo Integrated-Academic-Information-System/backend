6adbf4a02dfb9c90ade1ad8ea9932b36607acd8e
Udara Rathnayake <166277555+sandu2098@users.noreply.github.com>
2026-05-23 23:17:18 +0530

    Add backend

---
+++ app/Http/Controllers/Api/AcademicYearController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\AcademicYear;
+
+class AcademicYearController extends Controller {
+    public function index() {
+        return response()->json(AcademicYear::latest()->get());
+    }
+}
