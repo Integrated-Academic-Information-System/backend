8eed60e5c78e0198e5bcc99dc9fa3f50ec180d75
Udara Rathnayake <166277555+sandu2098@users.noreply.github.com>
2026-05-23 23:12:53 +0530

    Add backend

---
+++ app/Models/Alert.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Alert extends Model {
+    protected $fillable = ['title', 'message', 'role_target', 'created_by'];
+
+    public function creator() {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
