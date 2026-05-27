9f18aa5af2e48ab4440f290f5f9f917ccf593a9c
DCDS Karunanayaka <kdchamiladesilva@gmail.com>
2026-05-27 23:39:44 +0530

    Add migrations for terms and exam_years tables

---
+++ database/migrations/2026_05_27_180012_create_terms_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('terms', function (Blueprint $table) {
+            $table->id(); // from this create auto-incrementing 'id' INT PRIMARY KEY
+            $table->string('name', 10); // name VARCHAR(10)
+            $table->timestamps(); // Laravel default  for create_time, update_time TIMESTAMP  
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('terms');
+    }
+};
