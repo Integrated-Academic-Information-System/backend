ffda380950161932f67b4eba0478bd5e0d56698c
DCDS Karunanayaka <kdchamiladesilva@gmail.com>
2026-05-29 15:13:38 +0530

    Add migrations for bucket_subjects, subject_has_bucket_subject, teacher_bucket_subject, grade_has_sub_grade, teacher_has_grade, and student_has_marks tables

---
+++ database/migrations/2026_05_29_093944_create_student_has_marks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_has_marks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained('students')->onDelete('cascade');
+            $table->string('student_reg_no', 45); // Diagram eke hatiyata
+            $table->foreignId('marks_id')->constrained('marks')->onDelete('cascade');
+            $table->foreignId('grade_has_sub_grade_id')->constrained('grade_has_sub_grade')->onDelete('cascade');
+            $table->foreignId('subject_id')->constrained('subjects')->onDelete('cascade');
+            $table->foreignId('term_id')->constrained('terms')->onDelete('cascade');
+            $table->foreignId('exam_year_id')->constrained('exam_years')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_has_marks');
+    }
+};
