71b85a3eb05e4841ae081e27ba082aa9e70551b1
DCDS Karunanayaka <kdchamiladesilva@gmail.com>
2026-05-29 15:07:15 +0530

    Add migrations for admins, grades, sub_grades, marks, and teacher_has_subject tables

---
+++ database/migrations/2026_05_29_084014_create_marks_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('marks', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('mark', 5, 2); // DECIMAL(5,2) can put 100.00 marks
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('marks');
+    }
+};
