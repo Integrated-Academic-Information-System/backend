ad54ced2eea65c3ff5ccff81ebb66b011854fc80
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-06-10 23:01:35 +0530

    Refactor API routes to clean up commented code and ensure proper middleware usage

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 
 // A GET request/when come to students route, run getStudents function in StudentController to fetch all students from the database and return as JSON response to the frontend 
-Route::get('/students', [StudentController::class, 'getStudents']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/students', [StudentController::class, 'getStudents']);
+
+Route::middleware('auth:api')->group(function () {
+    
+    Route::post('/logout',  [AuthController::class, 'logout']);
+
+    // your protected routes here...
+});
