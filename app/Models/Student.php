0634037b3381bd8fc206fedc7c8614c58cf98075
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-06-11 11:43:08 +0530

    Refactor Student model to extend Authenticatable and implement JWTSubject for authentication

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,9 +2,37 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Student extends Model
+class Student extends Authenticatable implements JWTSubject
 {
-    //
+    protected $table = 'students';
+
+    protected $fillable = [
+        'reg_no',
+        'password',
+        'name',
+        'address',
+        'reg_date',
+        'leave_date',
+        'mobile_number',
+        'email',
+        'status',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    // Required for JWT if students can also login
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
