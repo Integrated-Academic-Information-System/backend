55273e37b0e18e251d991e27f6364fc38fbfce63
Dulari Dayananda <dulariwathsala824@gmail.com>
2026-07-25 22:06:06 +0530

    feat: create Grade and Term models with relationships

--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Grade extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+    ];
+
+    public function students(): HasMany
+    {
+        return $this->hasMany(Student::class);
+    }
+
+    public function marks(): HasMany
+    {
+        return $this->hasMany(Mark::class);
+    }
 }
--- app/Models/Term.php
+++ app/Models/Term.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Term extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'is_current',
+    ];
+
+    protected $casts = [
+        'is_current' => 'boolean',
+    ];
+
+    public function students(): HasMany
+    {
+        return $this->hasMany(Student::class);
+    }
+
+    public function marks(): HasMany
+    {
+        return $this->hasMany(Mark::class);
+    }
 }
